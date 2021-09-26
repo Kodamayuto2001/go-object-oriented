2f87f808c2cf4197cc401161e45953dd8e0aae64
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-26 21:11:48 +0900

    追加

---
+++ project19/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+	"crypto/md5"
+	"io"
+	"strconv"
+	"os"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!")
+}
+
+func login( w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("login.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			//	tokenの合法性を検証します。
+		} else {
+			//	tokenが存在しなければエラーを出します。
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)	//	リクエストを受け取るメソッド
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
